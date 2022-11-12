cf8f660577112135d2f82737ba0a3a08d9a3389b
Rosendo <rosenvb.global@gmail.com>
2022-11-11 19:56:35 -0600

    First Commit

---
+++ RM-API/Utils.swift
@@ -0,0 +1,24 @@
+//
+//  Utils.swift
+//  RM-API
+//
+//  Created by Rosendo Vazquez on 11/11/22.
+//
+
+import Foundation
+import UIKit
+
+public struct Utils{
+    
+    //MARK: API-URL
+    static let BASE_URL = "https://rickandmortyapi.com/api/character"
+    static let AVATAR_URL = "https://rickandmortyapi.com/api/character/avatar/"
+    
+    
+    //MARK: UI Colors
+    static let COLOR_BACKGROUND = UIColor(red: 0.07, green: 0.26, blue: 0.26, alpha: 1.00)
+    static let COLOR_TEXT = UIColor(red: 0.44, green: 0.82, blue: 0.82, alpha: 1.00)
+    
+    static let IMAGE_DIMENTION = 200
+    
+}
